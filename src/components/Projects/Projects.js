011f30dde02f83fd62c839270a9807c7dc8d1610
Tejas Gupta <2017tejasgupta@gmail.com>
2023-01-06 11:52:02 +0530

    completed projects

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -2,12 +2,12 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
-import leaf from "../../Assets/Projects/leaf.png";
-import emotion from "../../Assets/Projects/emotion.png";
-import editor from "../../Assets/Projects/codeEditor.png";
-import chatify from "../../Assets/Projects/chatify.png";
-import suicide from "../../Assets/Projects/suicide.png";
-import bitsOfCode from "../../Assets/Projects/blog.png";
+import getfilter from "../../Assets/Projects/getfilter.png";
+import rto from "../../Assets/Projects/RTO.png";
+import chellam from "../../Assets/Projects/chellam.png";
+import reader from "../../Assets/Projects/reader.png";
+import diabetes from "../../Assets/Projects/diabetes.png";
+import ddos from "../../Assets/Projects/ddos.png";
 
 function Projects() {
   return (
@@ -24,7 +24,7 @@ function Projects() {
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={chatify}
+              imgPath={getfilter}
               isBlog={false}
               title="get-filter"
               description="A website featuring different filters applied on an image. get-filter website helps user to use different filters on different images and your favourite ones can be downloaded in the form of png/jpg/jfif."
@@ -34,17 +34,17 @@ function Projects() {
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={bitsOfCode}
+              imgPath={rto}
               isBlog={false}
               title="RTO-NOTIFIER"
-              description="We have created RTO NOTIFIER Web Application to contact Vehicle's owner with just an image. This is cretaed using Flask framework and various computer vision libraries to detect car and extract the number plate. After retriving number, it checks the database and send a mail to the owner automatically."
+              description="Created RTO NOTIFIER Web Application to contact Vehicle's owner with just an image. This is cretaed using Flask framework and various computer vision libraries to detect car and extract the number plate. After retriving number, it checks the database and send a mail to the owner automatically."
               ghLink="https://github.com/tejas0207/RTO-NOTIFIER"
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={editor}
+              imgPath={chellam}
               isBlog={false}
               title="chellam-search-kubernetes"
               description="Web Application to automate our Kubernetes task. Using this, you don't need to remember any commands of Kubernetes , Just type as per your convenience in human language and our app will interpret it and do the needful stuffs for you."
@@ -55,7 +55,7 @@ function Projects() {
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={leaf}
+              imgPath={ddos}
               isBlog={false}
               title="DDoS-shield"
               description="Built a real-time DDOS attack detection and prevention script that on the fly check for malicious IP’s attacking the server on daily basis and blocking them if approved . It also shows total traffic on website(daily as well as overall)"
@@ -65,10 +65,10 @@ function Projects() {
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={suicide}
+              imgPath={diabetes}
               isBlog={false}
               title="flask_diabetes_prediction"
-              description="Using 'Natural Launguage Processing' for the detection of suicide-related posts and user's suicide ideation in cyberspace  and thus helping in sucide prevention."
+              description="It's a Web Application that works to detect if the person has diabetes or not, based on some inputs from user which is then inserted in the machine learning model to predict the outcome."
               ghLink="https://github.com/tejas0207/flask_diabetes_prediction"
 
             />
@@ -76,7 +76,7 @@ function Projects() {
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={emotion}
+              imgPath={reader}
               isBlog={false}
               title="Reader"
               description="It's a Web Application which deals with converting text to speech. Not only it is limited to text but also we can make it read from an image. It is well intergated with cloud services like Polly, textract, etc. to fulfil the requirements."
